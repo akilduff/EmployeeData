ed67ccd0b45b36c7bf13f1638d88964891474629
Andrew Kilduff <a.l.kilduff@gmail.com>
2021-01-28 09:26:56 -0800

    Box shadows, font changes, and other styling

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # EmployeeData
 
+This React app is used to search and filter a static employee database by name, department, and age.
+
+## Usage
+
+Begin typing in an employee's name to search by name. Filtering by department and age range is available in the drop down menus.
+
+## Requirements
+
+- Node 6.13.0
+
 ### Installing Dependencies
 
 From within the root directory:
--- client/components/EmployeeRender.jsx
+++ client/components/EmployeeRender.jsx
@@ -5,25 +5,34 @@ import styled from 'styled-components';
 const EmployeeRender = (props) => {
 
   const EmployeeStyle = styled.div`
-      display: inline-block;
-      text-align: center;
-      justify-content: center;
-      margin-top: 1rem;
-      margin-bottom: 1rem;
-      margin-right: 0.25rem;
-      margin-left: 0.25rem;
-      padding: 21px 9px 21px 9px;
-      border: 1px solid #ccc;
-      box-sizing: border-box;
-      border-radius: 4px;
+    display: inline-block;
+    text-align: center;
+    justify-content: center;
+    width: 250px;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    margin-right: 0.25rem;
+    margin-left: 0.25rem;
+    padding: 21px 9px 21px 9px;
+    background: #fed501;
+    border: 2px solid #363636;
+    box-shadow: 3px 3px 3px #363636;
+    border-radius: 4px;
+  `;
+
+  const EmployeeName = styled.div`
+    font-size: x-large;
+    font-weight: bold;
+    padding: 0px 0px 10px 0px;
   `;
 
   return (
     <div>{props.employeesToRender.map((employee) => (
       <div>
         <EmployeeStyle>
-          <h3>{employee.name}</h3>
-          <div>Department: {employee.department} // Age: {employee.age}</div>
+          <EmployeeName>{employee.name}</EmployeeName>
+          <div>Department: {employee.department}</div>
+          <div>Age: {employee.age}</div>
         </EmployeeStyle>
         <br></br>
       </div>
